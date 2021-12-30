2464f8bc557282be39a5b90bff5709b6b5fb72eb
dhanu Bhardwaj <bhardwajdhanu-09@gmail.com>
2021-12-31 00:04:22 +0530

    Adding the directory structure and install the express

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const port = 8990;
+const app = express();
+
+app.listen(port, function (error) {
+  if (error) {
+    console.log(`Error in connecting with server: ${error}`);
+  }
+  console.log(`Successfully connecting with server ${port}`);
+});
