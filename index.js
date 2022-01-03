4aada8ddced29956cc73c106ed804947302d7e85
dhanu Bhardwaj <bhardwajdhanu-09@gmail.com>
2022-01-04 04:47:11 +0530

    chnges minorly

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const port = process.env.PORT || 8990;
 const app = express();
-require("dotenv").config();
+
 const cors = require("cors");
 const db = require("./config/mongoose");
 
