dcbf570b9976a1490d2b5ab2419460877fa6c102
dhanu Bhardwaj <bhardwajdhanu-09@gmail.com>
2022-01-04 12:06:34 +0530

    chnges minorly db

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,26 +1,16 @@
 
 const mongoose = require('mongoose');
-
-// mongoose.connect('mongodb+srv://Kdatabase:Kdatabase@placement-cell-tracker.wl45o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
-
 const uri = process.env.MONGODB_URI || 'mongodb://localhost/placement-cell-tracker';
 
-mongoose
-  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("Connected to  database ");
-  })
-  .catch((err) => {
-    console.error(`Error connecting to the database. \n${err}`);
-  });
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// const db = mongoose.connection;
+const db = mongoose.connection;
 
-// db.on('error',console.error.bind(console,"Error connecting to MongoDB ......"));
+db.on('error',console.error.bind(console,"Error connecting to MongoDB ......"));
 
-// db.once('open',function(){
-//     console.log('Connected to Database :: Mongodb')
-// })
+db.once('open',function(){
+    console.log('Connected to Database :: Mongodb')
+})
 
 
-// module.exports = db;
+module.exports = db;
